f3324a1a831882fb262cf55094eb282d7c26a582
s64470 <s64470@bht-berlin.de>
2023-12-29 21:49:45 +0100

    minor changes

--- DjangoWebProject-TMT/app/views.py
+++ DjangoWebProject-TMT/app/views.py
@@ -5,7 +5,7 @@ Definition of views.
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from datetime import datetime
+from datetime import date, datetime
 from django.shortcuts import render
 from django.http import HttpRequest
 
@@ -22,36 +22,37 @@ def home(request):
         }
     )
 
-# contact page
-def contact(request):
-    """Renders the contact page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/contact.html',
-        {
-            'title':'Contact',
-            'message':'Your contact page.',
-            'year':datetime.now().year,
-        }
-    )
+# # contact page
+# def contact(request):
+#     """Renders the contact page."""
+#     assert isinstance(request, HttpRequest)
+#     return render(
+#         request,
+#         'app/contact.html',
+#         {
+#             'title':'Contact',
+#             'message':'Your contact page.',
+#             'year':datetime.now().year,
+#         }
+#     )
 
-# about page
-def about(request):
-    """Renders the about page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/about.html',
-        {
-            'title':'About',
-            'message':'Your application description page.',
-            'year':datetime.now().year,
-        }
-    )
+# # about page
+# def about(request):
+#     """Renders the about page."""
+#     assert isinstance(request, HttpRequest)
+#     return render(
+#         request,
+#         'app/about.html',
+#         {
+#             'title':'About',
+#             'message':'Your application description page.',
+#             'year':datetime.now().year,
+#         }
+#     )
 
 # sign up page
 def user_signup(request):
+    """Renders the about page."""
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -59,4 +60,12 @@ def user_signup(request):
             return redirect('login')
     else:
         form = UserCreationForm()
-    return render(request, 'app/signup.html', {'form': form})
+    return render(
+        request,
+        'app/signup.html',
+        {
+            'title':'User registration',
+            'form': form,
+            'year':datetime.now().year,
+        }
+    )
