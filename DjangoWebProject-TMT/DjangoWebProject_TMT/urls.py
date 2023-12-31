4ffcb73301a7b341d2474a886181e8c391004f1b
Denio <de.glowacki@ostfalia.de>
2023-12-31 11:03:52 +0100

    Delete View

--- DjangoWebProject-TMT/DjangoWebProject_TMT/urls.py
+++ DjangoWebProject-TMT/DjangoWebProject_TMT/urls.py
@@ -34,4 +34,5 @@ urlpatterns = [
     path('task/<int:pk>/', views.TaskDetail.as_view(), name='task'),
     path('task-create/', views.TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>/', views.TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', views.DeleteView.as_view(), name='task-delete'),
 ]
--- DjangoWebProject-TMT/app/views.py
+++ DjangoWebProject-TMT/app/views.py
@@ -12,7 +12,7 @@ from django.http import HttpRequest, HttpResponse
 from django.views.generic import detail
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -96,4 +96,9 @@ class TaskCreate(CreateView):
 class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
     success_url = reverse_lazy('tasks')
