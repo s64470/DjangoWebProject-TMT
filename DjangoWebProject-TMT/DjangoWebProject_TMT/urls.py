8bf82ecede92416ca9995526291df752493e35bd
Denio <de.glowacki@ostfalia.de>
2023-12-31 11:27:23 +0100

    combine login with tasks | not user bounded, only on user logged in

--- DjangoWebProject-TMT/DjangoWebProject_TMT/urls.py
+++ DjangoWebProject-TMT/DjangoWebProject_TMT/urls.py
@@ -9,24 +9,24 @@ from app import forms, views
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
-
 # path to .html files
 urlpatterns = [
     path('', views.home, name='home'),
     # path('contact/', views.contact, name='contact'),
     # path('about/', views.about, name='about'),
-    path('login/',
+   path('login/',
          LoginView.as_view
-         (
+             (
              template_name='app/login.html',
              authentication_form=forms.BootstrapAuthenticationForm,
              extra_context=
              {
                  'title': 'Log in',
-                 'year' : datetime.now().year,
+                 'year': datetime.now().year,
              }
          ),
          name='login'),
+    # path('login/', LoginView.as_view(template_name='app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('admin/', admin.site.urls),
     path('signup/', views.user_signup, name='signup'),
--- DjangoWebProject-TMT/app/views.py
+++ DjangoWebProject-TMT/app/views.py
@@ -14,6 +14,9 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView, LogoutView
+
 from .models import Task
 
 
@@ -25,10 +28,11 @@ def home(request):
         request,
         'app/index.html',
         {
-            'title':'Home Page',
-            'year':datetime.now().year,
+            'title': 'Home Page',
+            'year': datetime.now().year,
         }
     )
+
 
 # # contact page
 # def contact(request):
@@ -72,33 +76,48 @@ def user_signup(request):
         request,
         'app/signup.html',
         {
-            'title':'User registration',
-            'form':form,
-            'year':datetime.now().year,
+            'title': 'User registration',
+            'form': form,
+            'year': datetime.now().year,
         }
     )
+
 
 # task list
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
-    
+
+
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'app/task.html'
+
 
 class TaskCreate(CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
+
 class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
 
+
 class DeleteView(DeleteView):
     model = Task
     context_object_name = 'task'
     success_url = reverse_lazy('tasks')
+
+"""
+class CustomLoginView(LoginView):
+    template_name = 'app/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+    
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+"""
