2539808c4abdec006f14386a8813449c8c850728
s64470 <s64470@bht-berlin.de>
2024-01-13 23:57:16 +0100

    styling user registration from

--- DjangoWebProject-TMT/app/templates/app/forms.py
+++ DjangoWebProject-TMT/app/templates/app/forms.py
@@ -7,16 +7,24 @@ from django.contrib.auth.models import User
 #      class Meta:
 #          model = User
 #          fields = ['username', 'password1', 'password2']
-         
+
 
 class RegisterUserForm(UserCreationForm):
-    email = forms.EmailField()
-    username = forms.CharField(max_length=50)
-    
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={'class': 'form-control'}))
+    username = forms.CharField(
+        max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-   
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterUserForm, self).__init__(*args, **kwargs)
+
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
