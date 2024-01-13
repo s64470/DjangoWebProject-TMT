60cf38a67dd69822cf6237c7184aac9e22410e81
s64470 <s64470@bht-berlin.de>
2024-01-13 21:47:33 +0100

    code cleanup

--- DjangoWebProject-TMT/DjangoWebProject_TMT/urls.py
+++ DjangoWebProject-TMT/DjangoWebProject_TMT/urls.py
@@ -3,9 +3,8 @@ Definition of urls for DjangoWebProject_TMT.
 """
 
 from datetime import datetime
-import token
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView, PasswordResetCompleteView, PasswordResetConfirmView, PasswordResetDoneView
+from django.contrib.auth.views import LoginView, LogoutView
 from app import forms, views
 from django.urls import path
 from django.contrib.auth import views as auth_views
@@ -32,9 +31,13 @@ urlpatterns = [
     path('task-create/', views.TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>/', views.TaskUpdate.as_view(), name='task-update'),
     path('task-delete/<int:pk>/', views.DeleteView.as_view(), name='task-delete'),
-    
-    path('password_reset_form/', auth_views.PasswordResetView.as_view(), name='password_reset_form'),
-    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+    path('password_reset_form/', auth_views.PasswordResetView.as_view(),
+         name='password_reset_form'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
 ]
