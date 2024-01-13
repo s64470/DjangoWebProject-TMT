be83d7b6b92612e0ee522c5e7aecbf3e0ffcae8a
s64470 <s64470@bht-berlin.de>
2024-01-13 22:29:10 +0100

    setting up password recovery functionality

--- DjangoWebProject-TMT/DjangoWebProject_TMT/settings.py
+++ DjangoWebProject-TMT/DjangoWebProject_TMT/settings.py
@@ -30,13 +30,12 @@ DEBUG = config('DEBUG', cast=bool)
 ALLOWED_HOSTS = []
 
 # EMAIL settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
